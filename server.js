4ef2b02276c0c2b0235ab8a8db93db6e7db862c1
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-06 15:04:32 -0400

    Add endpoint to get user by ID

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ const users = [
 
 server.get("/api/users", (req, res) => res.json(users));
 
+server.get("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    const user = users.find(user => user.id == id);
+    user ? res.status(200).json(user) : res.status(404).json({message: "User not found"});
+});
+
 const port = 5000;
 
 server.listen(port, () => console.log(`\n API on port ${port} is working`));
